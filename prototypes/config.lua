1679288e2c32abbc6809917afd5629ace3e3448e
Sebastian Kaim <sebb@sebb767.de>
2016-03-18 21:55:15 +0100

    Refactored config

--- prototypes/config.lua
+++ prototypes/config.lua
@@ -1,12 +1,8 @@
 MOD_NAME = "__more-locomotives__"
 
+-- Whether to let the cargo locomotive accelerate slowly. This makes it less useful for short-distance 
+-- transport or stop-and-go, but on the other hand, slowly accelerating locomotives are awesome!
+CARGOLOC_SLOW_ACCELERATION = false
+
 --  Leftover from color coding. I'm not sure if its still used anywhere, so I'll keep it for reference.
 COLOR_TABLE = {}
-
-
--- Some of those blocks are still in the code so that I can use the code for later reference. This should however disable its use
-ENABLE_TRAINS = false
-ENABLE_LAMPS = false
-ENABLE_CONCRETE = false
-
---
