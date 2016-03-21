0f56d88cf409e89bda3f653386eec09c3f5eac77
Sebastian Kaim <sebb@sebb767.de>
2016-03-21 12:23:52 +0100

    Added electro locomotive item

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -15,4 +15,10 @@ locomotive.icon = MOD_NAME.."/graphics/locomotive/red/icon.png"
 locomotive.place_result = "hs-locomotive"
 table.insert(t_items,locomotive)
 
+locomotive = util.table.deepcopy(data.raw["item"]["diesel-locomotive"])
+locomotive.name = "electro-locomotive"
+locomotive.icon = MOD_NAME.."/graphics/locomotive/blue/icon.png"
+locomotive.place_result = "electro-locomotive"
+table.insert(t_items,locomotive)
+
 data:extend(t_items)
